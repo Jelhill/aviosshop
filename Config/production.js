f13870fe90276216db626b91b9fc4a9a59975e3e
Jelhill <talk2jelili2000@gmail.com>
2021-01-17 16:23:23 +0100

    update

--- Config/production.js
+++ Config/production.js
@@ -1,26 +1,65 @@
 const mysql = require("mysql")
 require("dotenv").config()
 
-mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER, 
     password: process.env.PASSWORD,
-    port: 3306,
+    ssl: false,
     database: process.env.DB,
 })
 
-mysqlConnection.connect((err) => {
-    console.log("Port", process.env.PORT, process.env.HOST, process.env.USER, process.env.PASSWORD)
+
+mysqlConnection.getConnection((err, conn) => {
     if(err) {
         console.log(err)
         console.log("connection failed")
     }else{
-        console.log(process.env.PORT)
         console.log("!!!Database connected")
         const app = require("../server")
         const PORT = process.env.PORT || 4000
         app.listen(PORT, () => console.log(`::Server running on port ${PORT}`))
     }
-})
+});
+
+// mysqlConnection.connect((err) => {
+//     console.log("Port", process.env.PORT, process.env.HOST, process.env.USER, process.env.PASSWORD)
+//     if(err) {
+//         console.log(err)
+//         console.log("connection failed")
+//     }else{
+//         console.log("!!!Database connected")
+//         const app = require("../server")
+//         const PORT = process.env.PORT || 4000
+//         app.listen(PORT, () => console.log(`::Server running on port ${PORT}`))
+//     }
+// })
+
+
+// const mysqlConnection = mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.USER, 
+//     password: process.env.PASSWORD,
+//     ssl: false,
+//     database: process.env.DB,
+// })
+
+
+// mysqlConnection.connect((err) => {
+//     console.log("Port", process.env.PORT, process.env.HOST, process.env.USER, process.env.PASSWORD)
+//     if(err) {
+//         console.log(err)
+//         console.log("connection failed")
+//     }else{
+//         console.log("!!!Database connected")
+//         const app = require("../server")
+//         const PORT = process.env.PORT || 4000
+//         app.listen(PORT, () => console.log(`::Server running on port ${PORT}`))
+//     }
+// })
+
+// mysqlConnection.on('error', function(err) {
+//     console.log(err.code); // 'ER_BAD_DB_ERROR'
+// });
 
 module.exports = mysqlConnection
